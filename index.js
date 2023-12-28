e9b6ff87c042c355fb80003c5eaecdfffd4a46df
SonLT <sonlt@thefirstone.jp>
2023-12-28 17:34:50 +0700

    update

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 const { WebClient } = require(slack/web-api);
 
@@ -20,6 +21,9 @@ const slackEvents = createEventAdapter(slackSigningSecret);
 const slackToken = process.env.SLACK_BOT_TOKEN;
 
 const slackClient = new WebClient(slackToken);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.post('/translate', function (req, res) {
   const { challenge } = req.body;

982d01d265f5829762363c82da086c95e1f7208e
SonLT <sonlt@thefirstone.jp>
2023-12-29 13:05:37 +0700

    update

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const axios = require('axios');
 
 require('dotenv').config();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 const apiKey = process.env.API_KEY ;
 

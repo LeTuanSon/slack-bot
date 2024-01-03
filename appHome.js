300b6304f280cbf66a5043812f1b8b3434bb31d8
SonLT <sonlt@thefirstone.jp>
2024-01-03 15:26:06 +0700

    update

--- appHome.js
+++ appHome.js
@@ -9,6 +9,7 @@ var mysql = require('mysql');
 
 var connection = mysql.createConnection({
     host: "localhost",
+    port: 3306,
     user: "root",
     password: "sonlt1234",
     database: "sakila"
